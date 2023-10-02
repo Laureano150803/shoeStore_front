899fe8ea241e978fba8c4a757ad850be28ff9c25
Laureano Hurtado <laureanohurtado1@gmail.com>
2023-10-01 22:51:50 -0500

    fixig request api

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-let apiUrl = 'http://localhost:8000/'
+let apiUrl = 'http://localhost:8000/';
 
-if(import.meta.env.VITE_API === 'production'){
-    apiUrl == import.meta.env.VITE_API
-} 
+if (import.meta.env.VITE_API === 'production') {
+    apiUrl = import.meta.env.VITE_API;
+}
 
-export default apiUrl
+export default apiUrl;
