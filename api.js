237b3167fd1366e6272da77ade62fce44e4e636f
Laureano Hurtado <laureanohurtado1@gmail.com>
2023-10-02 08:15:53 -0500

    adding more things

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-let apiUrl = 'https://shoe-store-cezh.onrender.com';
+let apiUrl = 'http://localhost:8000/';
 
-/* if (import.meta.env.VITE_API === 'production') {
+if (import.meta.env.VITE_API === 'production') {
     apiUrl = import.meta.env.VITE_API;
-} */
+} 
 
 export default apiUrl;
